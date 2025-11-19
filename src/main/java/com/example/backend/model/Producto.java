81956c9867d25b56b193d74316ade6ca456d0332
Constanza Raquel Pino Villa <co.pinov@duocuc.cl>
2025-11-19 14:07:41 -0300

    cosas de rama otra

---
+++ src/main/java/com/example/backend/model/Producto.java
@@ -0,0 +1,28 @@
+package com.example.backend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "productos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Long id; 
+
+    @Column (nullable=false, unique = true, length = 13)
+    private String nombreProducto; 
+
+    @Column (nullable=false, length=100)
+}
