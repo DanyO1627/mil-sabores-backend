2c874a9df08739ac468179e8c4f4c473519ce1af
Dany01627 <dani.oliveros@duocuc.cl>
2025-11-26 14:29:11 -0300

    edito backendAplication y Usuario Repository

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
 
 import org.springframework.context.annotation.Bean;
-
 import com.example.backend.model.Usuario;
 import com.example.backend.repository.UsuarioRepository;
 
--- src/main/java/com/example/backend/repository/UsuarioRepository.java
+++ src/main/java/com/example/backend/repository/UsuarioRepository.java
@@ -1,9 +1,6 @@
 package com.example.backend.repository;
 
-
-
-
-import com.example.backend.model.Usuario;
+import com.example.backend.model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
